46e7d04650e8d7f537115e5669fe182856ee3252
RodrigoAraP <133711269+RodrigoAraP@users.noreply.github.com>
2023-09-03 15:55:05 -0300

    Delete Pratica1_OO/src directory

--- Pratica1_OO/src/Cliente.java
+++
@@ -1,7 +0,0 @@
-public class Cliente {
-    private double cpf;
-    private String nome;
-    private double divida;
-    private double parcela;
-
-}
--- Pratica1_OO/src/Transacao.java
+++
@@ -1,4 +0,0 @@
-public class Transacao {
-    private double valor;
-
-}
