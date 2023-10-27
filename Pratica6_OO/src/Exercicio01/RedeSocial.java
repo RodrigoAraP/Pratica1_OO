1c8db1bec75cc6c3b9694773df6b690febfca2a6
RodrigoAraP <133711269+RodrigoAraP@users.noreply.github.com>
2023-10-26 21:58:05 -0300

    Add files via upload

---
+++ Pratica6_OO/src/Exercicio01/RedeSocial.java
@@ -0,0 +1,23 @@
+package Exercicio01;
+
+import java.util.ArrayList;
+
+public class RedeSocial {
+    private String dataCriacao;
+    private String nomeUsuario;
+    private String dataNascimento;
+    private String senha;
+
+    ArrayList<Publicacao> publicacoes = new ArrayList<>();
+
+    public RedeSocial(String dataCriacao, String nomeUsuario, String dataNascimento, String senha) {
+        this.dataCriacao = dataCriacao;
+        this.nomeUsuario = nomeUsuario;
+        this.dataNascimento = dataNascimento;
+        this.senha = senha;
+    }
+    public void inserePublicacao(Publicacao publi){
+
+    }
+
+}
